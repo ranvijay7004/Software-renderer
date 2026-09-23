bb0b1089b9359b62ec56e7e88f28cbd5a1f1c0a2
ranvijay7004 <ranvijaykumararyanew@gmail.com>
2026-09-23 15:54:28 +0530

    Switched to sfml and first window opened

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,26 @@
-#define SDL_MAIN_HANDLED
-#include <SDL.h>
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-    SDL_SetMainReady();
+int main(){
 
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cerr << "SDL_Init failed: " << SDL_GetError() << "\n";
-        return 1;
-    }
+    sf::RenderWindow window(sf::VideoMode(800,600),"Window");
+    
+while(window.isOpen()){
 
-    SDL_Window* window = SDL_CreateWindow(
-        "Software Renderer",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        800, 600,
-        SDL_WINDOW_SHOWN
-    );
+    sf::Event event;
 
-    if (window == nullptr) {
-        std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << "\n";
-        SDL_Quit();
-        return 1;
-    }
+    while(window.pollEvent(event)){
+        if(event.type == sf::Event::Closed){
+            window.close();
+        }
 
-    bool running = true;
-    SDL_Event event;
-
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) running = false;
-            if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) running = false;
+        if(event.type == sf::Event::KeyPressed){
+            if(event.key.code == sf::Keyboard::Escape){
+                window.close();
+            }
         }
     }
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    window.display();
+}
     return 0;
 }
