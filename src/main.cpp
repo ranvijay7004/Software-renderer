7cb0dc44d47e0fb77543c7de953c9dc885032fb7
ranvijay7004 <ranvijaykumararyanew@gmail.com>
2026-09-23 23:40:10 +0530

    First pixels drwan,learned framebuffer and phase 1 completed

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
 #include <SFML/Graphics.hpp>
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+void setpixel(std::vector<uint8_t>& framebuffer , int x , int y ,
+    uint8_t r, uint8_t g, uint8_t b, uint8_t a ){
+       
+    int index = (y * WIDTH + x) * 4;
+
+    framebuffer[index + 0] = r;
+    framebuffer[index + 1] = g;
+    framebuffer[index + 2] = b;
+    framebuffer[index + 3] = a;
+}
+
+
 int main(){
 
-    sf::RenderWindow window(sf::VideoMode(800,600),"Window");
+    sf::RenderWindow window(sf::VideoMode(WIDTH,HEIGHT),"Window");
+
+    std::vector<uint8_t> framebuffer(WIDTH*HEIGHT*4);
     
+    for(int i = 0 ; i < 600 ; i++){
+    setpixel(framebuffer,50,i,225,0,0,225);
+    }
+
+    sf::Texture pixel;
+    pixel.create(WIDTH,HEIGHT); 
+    pixel.update(framebuffer.data());
+    sf::Sprite pixelsprite; 
+    pixelsprite.setTexture(pixel);
+
+
 while(window.isOpen()){
 
     sf::Event event;
@@ -20,6 +48,8 @@ while(window.isOpen()){
         }
     }
 
+    window.clear();
+    window.draw(pixelsprite);
     window.display();
 }
     return 0;
